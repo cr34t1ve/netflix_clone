61356927ce866ef144165f9364699840cb058e5a
cr34t1ve <mrsofua@gmail.com>
2022-03-30 12:29:57 +0000

    upcoming page initialised

--- lib/screens/upcoming.dart
+++ lib/screens/upcoming.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/size_config.dart';
 
 class Upcoming extends StatefulWidget {
   const Upcoming({Key? key}) : super(key: key);
@@ -10,6 +11,48 @@ class Upcoming extends StatefulWidget {
 class _UpcomingState extends State<Upcoming> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        shrinkWrap: true,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: getProportionateScreenWidth(20.0),
+              ),
+              Text(
+                'Upcoming',
+                style: TextStyle(
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )
+            ],
+          ),
+          ListView.separated(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Container(
+                      height: 30.0, width: 30.0, child: Placeholder()),
+                  title: Text(
+                    'Title',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  trailing: Icon(
+                    Icons.play_circle_outline_outlined,
+                    color: Colors.white,
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(color: Colors.white.withOpacity(0.3));
+              },
+              itemCount: 5)
+        ],
+      ),
+    );
   }
 }
