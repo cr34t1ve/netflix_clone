b5c1ced641b325be6db8ef7d52a2f67a3340f71a
cr34t1ve <mrsofua@gmail.com>
2022-04-01 01:49:53 +0000

    download button isolated

--- lib/components/horizontal_movie_list.dart
+++ lib/components/horizontal_movie_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:netflix_clone/components/download_button.dart';
 import 'package:netflix_clone/components/modal_sheet_content.dart';
+import 'package:netflix_clone/components/play_button.dart';
 
 import '../size_config.dart';
 
@@ -148,80 +150,6 @@ class _HorizontalMovieListState extends State<HorizontalMovieList> {
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class DownloadButton extends StatelessWidget {
-  const DownloadButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        height: getProportionateScreenHeight(40.0),
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(5)),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.play_arrow,
-                color: Colors.black,
-              ),
-              SizedBox(
-                width: getProportionateScreenWidth(3.0),
-              ),
-              Text(
-                'Play',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class PlayButton extends StatelessWidget {
-  const PlayButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        height: getProportionateScreenHeight(40.0),
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(5)),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.play_arrow,
-                color: Colors.black,
-              ),
-              SizedBox(
-                width: getProportionateScreenWidth(3.0),
-              ),
-              Text(
-                'Play',
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
